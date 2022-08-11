d5da90561104815f3f0361a818058b0a6c539f16
Waleed-Rafi <waleed.rafi626@gmail.com>
2022-08-12 00:48:27 +0500

    Routing setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import "./App.css";
 import Login from "./Screens/Auth/Login/Login";
 import SignUp from "./Screens/Auth/Signup/Signup";
 import Home from "./Screens/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      {/* <SignUp /> */}
-      {/* <Home /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<SignUp />} />
+
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
