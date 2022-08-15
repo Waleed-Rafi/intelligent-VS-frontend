9ff726b6178a2180aa3b235e2c3585e768c8c87d
Waleed-Rafi <waleed.rafi626@gmail.com>
2022-08-15 12:15:34 +0500

    new commit

--- src/Screens/Studio/SimpleUpload/SimpleUpload.jsx
+++ src/Screens/Studio/SimpleUpload/SimpleUpload.jsx
@@ -1,18 +1,51 @@
 import React from "react";
 import AppFileUpload from "../../../Components/AppFileUpload/AppFileUpload";
 import AppHeader from "../../../Components/AppHeader/AppHeader";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { useState } from "react";
+import axios from "../../../axios/index";
 const storage = getStorage();
 
 export default function SimpleUpload() {
-  const onFileChangeHandlerF = (file) => {
-    const timeStamp = new Date().getTime();
+  const [myFile, setMyFile] = useState(null);
+  const onFileChangeHandlerF = (e) => {
+    const file = e.target.files[0];
+    setMyFile(file);
+  };
 
-    const storageRef = ref(storage, "/simple-videos/" + timeStamp);
-    // 'file' comes from the Blob or File API
-    uploadBytes(storageRef, file).then((snapshot) => {
-      console.log("Uploaded a blob or file!");
-    });
+  const onVideoUploadHandler = () => {
+    if (myFile) {
+      const timeStamp = new Date().getTime();
+
+      const storageRef = ref(storage, "/simple-videos/" + timeStamp);
+
+      const uploadTask = uploadBytesResumable(storageRef, myFile);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const prog = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          console.log(prog);
+        },
+        (err) => console.log(err),
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+            console.log(url);
+            axios.post("/upload/video/simple", {
+              videoUrl: url,
+              userId: 1,
+            });
+          });
+        }
+      );
+    }
   };
   return (
     <div>
@@ -20,12 +53,21 @@ export default function SimpleUpload() {
       <div className="studio-face-blur-container">
         <div className="heading-section-main">
           <div className="face-blur-heading">Upload</div>
-          <div className="next-btn">Upload</div>
+          <div className="next-btn" onClick={onVideoUploadHandler}>
+            Upload
+          </div>
         </div>
         <div className="face-blur-video-image-main">
-          <AppFileUpload
+          {/* <AppFileUpload
             mainTitle="Upload a Video"
             onFileChangeHandler={onFileChangeHandlerF}
+          /> */}
+          <input
+            type="file"
+            name="Upload Video"
+            id=""
+            onChange={onFileChangeHandlerF}
+            accept=".mp4"
           />
         </div>
       </div>
